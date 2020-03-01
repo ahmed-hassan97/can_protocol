771afe0cbcae31acbc32ff82527e10fe3855a80e
ahmedhassan97 <ahmed1997abohassan@gmail.com>
2020-03-01 22:14:23 +0200

    add can driver for stm32 discovery_board

---
+++ can_driver/can_driver/can_driver.h
@@ -0,0 +1,68 @@
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+
+
+
+/**
+ * @addtogroup can
+ * @{
+ */
+
+/**
+ * @defgroup can_exported_typedefs
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup can_exported_defines
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup can_exported_macros
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup can_exported_constants
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup can_exported_functions
+ * @{
+ */
+
+
+ void CAN1_Init(void);
+ void CAN1_Receive(void);
+ void CAN1_Transmit(void);
+ void gpio_init();
+
+
+ /**
+  * @}
+  */
+ /**
+  * @}
+  */
+ /**
+  * @}
+  */
+
---
+++ can_driver/can_driver/can_driver_config.h
@@ -0,0 +1,22 @@
+/*
+ * can_driver_config.h
+ *
+ *  Created on: Feb 14, 2020
+ *      Author: A
+ */
+
+#ifndef CAN_DRIVER_CONFIG_H_
+#define CAN_DRIVER_CONFIG_H_
+
+
+/**
+ * @brief   AF9 PA11 and PA12 pin masks
+ */
+#define GPIO_AFRH_AFRH11                     ((uint32_t) 0x0000F000)
+#define GPIO_AFRH_AFRH11_AF9                 ((uint32_t) 0x00009000)
+#define GPIO_AFRH_AFRH12                     ((uint32_t) 0x000F0000)
+#define GPIO_AFRH_AFRH12_AF9                 ((uint32_t) 0x00090000)
+
+
+
+#endif /* CAN_DRIVER_CONFIG_H_ */
